ccd1c2a2fc01b74c421ea6620c4f62233df1bae6
Ravishankar Shetgar <ravishankar.shetgar@truckx.com>
2023-12-02 14:16:39 +0530

    Added profile pic

---
+++ src/components/ProfilePic/ProfilePic.styles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    height: 45,
+    width: 45,
+    borderWidth: 1,
+    borderRadius: 100,
+  },
+});
+
+export default styles;
--- src/screens/TimeTable/TimeTable.styles.ts
+++ src/screens/TimeTable/TimeTable.styles.ts
@@ -2,6 +2,13 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {},
+  headerView: {
+    borderWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 12,
+    alignItems: 'center',
+  },
 });
 
 export default styles;
